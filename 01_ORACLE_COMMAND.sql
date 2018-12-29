9bed987605ae86983fdbc2bcf38522d06be8e186
glory0510 <240949806@qq.com>
2018-12-29 17:50:10 +0800

    update 01_ORACLE_COMMAND.sql

--- 01_ORACLE_COMMAND.sql
+++ 01_ORACLE_COMMAND.sql
@@ -1,19 +1,12 @@
 远程桌面连接 win+r --> mstsc   3389
 
-SVN URL : https://192.168.2.9:8443/svn/r1/atreasury/ATS_PORJECT/10 Document/01 Input/Haier/170210HAIER-YK
-
-Jenkins URL：http://192.168.2.12:8080/jenkins
-jenkins web登录用户：admin/Zi5xian , sdg/wkopny976  ,  developer/developer
-jenkins安装服务器：192.168.2.12  jenkins/jenkins
 Start Jenkins
 ----------------------
 /opt/jenkins/SOFTWARE/apache-tomcat-7.0.59/bin/startup.sh
 
-
 Stop Jenkins
 ----------------------
 /opt/jenkins/SOFTWARE/apache-tomcat-7.0.59/bin/shutdown.sh
-
 
 Start Nginx
 ---------------------
@@ -27,35 +20,56 @@ cd /opt/jenkins/SOFTWARE/nginx/sbin
 ./nginx -s stop
 
 
-企微云第三方：超级管理员：zhenyi.hu@meridian.com.cn 密码zi5xian@Shengyin
+------------------------------------------------------
+--TO_CHAR 用法 转换的字段必须为时间类型
+SELECT TO_CHAR(SYSDATE,'HH24:Mi:SS') FROM DUAL;
 
-盛银服务器：
-172.20.0.40  root/zi5xian
-172.20.0.20  root/zi5xian$vm
-172.20.0.30  root/rootroot
-172.20.0.48 用户名是administrator，密码是test123！@#
+--TO_DATE用法 转换的字段必须为字符串类型
+SELECT TO_DATE('20171203 13:30:00','YYYYMMDD HH24:Mi:SS') FROM DUAL;
+--TO_TIMESTAMP用法 转换的字段必须为字符串类型
+SELECT TO_TIMESTAMP(LOGIN_TIME,'YYYYMMDD HH24:Hi:SS.ff')
+  FROM NHD.CONNECT_EVENT;
+  
+--REPLACE(COLUMN_1,'old_word','new_word')
+SELECT REPLACE(GROUP_NAME,'ABCI','中国农行') GROUP_NAME 
+  FROM LCM.LCM_USERS
+  WHERE GROUP_NAME='ABCI' ;
 
-###################
-http://nexus.teamlinker.net/index.html#welcome
-nexus_admin/Zi5xian
+--ROWNUM用法
+SELECT * 
+  FROM (SELECT * FROM LCM.LCM_USER_IN_OUT ORDER BY LOGIN_IN_TIME DESC)
+  WHERE ROWNUM=1 
+  
+--ROW_NUMBER() OVER (PARTITION COLUMN_1 ORDER BY COLUMN_2) 用法PARTITION COLLUMN是可选项
+SELECT *
+  FROM (SELECT GROUP_NAME,
+               LOGIN_IN_TIME,
+               USER_TYPE,
+               ROW_NUMBER() OVER( PARTITION BY GROUP_NAME ORDER BY LOGIN_IN_TIME DESC)
+        FROM LCM.LCM_USER_IN_OUT )
 
+--GROUP BY HAVING 用法 SELECT 后的字段和GROUP BY 的字段相同
+SELECT GROUP_NAME,ROLE_NUM,COUNT(*)
+  FROM LCM.LCM_USERS
+  GROUP BY GROUP_NAME,ROLE_NUM
+  HAVING COUNT(*) >=1
+  
+--SUBSTR (COLUMN,POSITION,LEN)
+SELECT DISTINCT (UPPER(SUBSTR(LOGINID,LENGTH(LOGINID) - 3, 4))) FROM LCM.LCM_USERS ;
 
-uat 打包log logachive  data 目录不用打包
+--ROWID 用法
+SELECT USERNAME,ROWID FROM LCM.LCM_USERS ;
 
-修改配置文件
-hosts
-control/
-.bash_profile
+--IN  和 ONT IN  用法
 
-
-pbox
-接入报价对接入数据进行处理
-
-quoteser 
-
-RMDSConnect_new.ini
-FXP【PSSVr】  nohup ./start_pricingserver_ceda.sh 2>&1 &
-
-update PAIRKEYS set SOURCE_SHORTNAME='ELEKTRON_DD';
-update POINTKEYS set SOURCE_SHORTNAME='ELEKTRON_DD';
-update RCV_DATA_CFG_TBL set EXT_SVC_NM='ELEKTRON_DD';
+/*      --EXISTS  和 NOT EXISTS 用法
+    强调的是否返回结果，不要求知道返回什么
+    SELECT 2 FROM STUDENT WHERE ...
+    |-----------------------------------------|
+    |表A                |表B                  |
+    |ID       NAME      |ID     AID     NAME  |
+    |1        A1        |1      1       B1    |
+    |2        A2        |2      2       B2    |
+    |3        A3        |3      2       B3    |
+    |-----------------------------------------|
+    
